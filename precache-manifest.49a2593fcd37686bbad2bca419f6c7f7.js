04955685b1b3c528372f305e0bb05006707b8e9a
Arnab <arnabdatta3@gmail.com>
2020-07-10 23:06:54 -0400

    Updates

---
+++ precache-manifest.49a2593fcd37686bbad2bca419f6c7f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0759d3882cf88f79d5e9205bbbb1944c",
+    "url": "/the-martian-daily/index.html"
+  },
+  {
+    "revision": "7017018955ae82ae8954",
+    "url": "/the-martian-daily/static/css/main.7822fec3.chunk.css"
+  },
+  {
+    "revision": "a01c276f6b96dfd01f0a",
+    "url": "/the-martian-daily/static/js/2.29c4b7de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/the-martian-daily/static/js/2.29c4b7de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7017018955ae82ae8954",
+    "url": "/the-martian-daily/static/js/main.c14c0635.chunk.js"
+  },
+  {
+    "revision": "3fd2c38ae05db600be42",
+    "url": "/the-martian-daily/static/js/runtime-main.d47a7a22.js"
+  }
+]);
